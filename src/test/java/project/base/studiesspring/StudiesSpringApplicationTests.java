e4de44241137f999ec9b5892a2d52fba4a51744a
leo-demetrio <leocdemetrio@gmail.com>
2022-06-17 10:24:05 -0300

    remove @SpringBootTest

--- src/test/java/project/base/studiesspring/StudiesSpringApplicationTests.java
+++ src/test/java/project/base/studiesspring/StudiesSpringApplicationTests.java
@@ -3,10 +3,10 @@ package project.base.studiesspring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StudiesSpringApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
